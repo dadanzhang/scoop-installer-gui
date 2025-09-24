97e2cc6b98a8301641d4ee4637ca1261b853a718
dadanzhang <qilong2.wang@gmial.com>
2025-09-24 16:00:37 +0800

    update err

--- Scoop安装工具.bat
+++ Scoop安装工具.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @echo off
 chcp 65001 >nul
 echo ========================================
@@ -7,14 +6,4 @@ echo ========================================
 echo.
 echo 正在启动...
 powershell -ExecutionPolicy Bypass -File "%~dp0ScoopInstaller.ps1"
-=======
-@echo off
-chcp 65001 >nul
-echo ========================================
-echo    Scoop 开发工具一键安装工具
-echo ========================================
-echo.
-echo 正在启动...
-powershell -ExecutionPolicy Bypass -File "%~dp0ScoopInstaller.ps1"
->>>>>>> 3d33965a12690bd8e6fdb301f45643e2f57e0ac3
 pause
